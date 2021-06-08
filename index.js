4a1765c7397607b7aba57582b05327055861f09e
Valen-prog <79101557+Valen-prog@users.noreply.github.com>
2021-06-08 20:05:05 +0200

    update

--- index.js
+++ index.js
@@ -386,7 +386,7 @@ client.on('message' , (message) =>
         .setDescription(`5-toubun no Hanayome :female:\n_Animanga roulette_ · 1009\nClaim Rank: #21\nLike Rank: #30\nNakano Miku`)
         .setImage(`https://images-ext-1.discordapp.net/external/IsFaoacboZNuzrKp-jrPqYrUN-KgT5EDqfBupmBpURo/https/media.discordapp.net/attachments/472313197836107780/747636059499921448/eYM3oYp.png`)
         .setFooter(`Belongs to 4Channer promedio`)
-        message.channel.send(`Wished by <@822854239087886346>, <@402521092796710915>`,{embed: frembed})
+        message.channel.send(`Wished by <@718998542030733362>, <@402521092796710915>`,{embed: frembed})
     }
     
     function help(){
