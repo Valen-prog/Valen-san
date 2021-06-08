9ce76054a43ee2f6028d8a1df183fd1053f90370
Valen-prog <79101557+Valen-prog@users.noreply.github.com>
2021-06-08 21:28:52 +0200

    save

--- index.js
+++ index.js
@@ -385,7 +385,7 @@ client.on('message' , (message) =>
         .setDescription(`**Miku Nakano** \n \n5-toubun no Hanayome ♀️\n_Animanga roulette_ · 1009\nClaim Rank: #21\nLike Rank: #30\nNakano Miku`)
         .setImage(`https://images-ext-1.discordapp.net/external/IsFaoacboZNuzrKp-jrPqYrUN-KgT5EDqfBupmBpURo/https/media.discordapp.net/attachments/472313197836107780/747636059499921448/eYM3oYp.png`)
         .setFooter(`Belongs to 4Channer promedio`)
-        message.channel.send(`Wished by <@718998542030733362>, <@402521092796710915>`,{embed: frembed}).then((msg)=> {msg.edit(`Wished by <@718998542030733362>, <@402521092796710915>`).react("❤")})}
+        message.channel.send(`Wished by <@718998542030733362>, <@402521092796710915>`,{embed: frembed}).react("❤")}
     
     function help(){
         const embed = new Discord.MessageEmbed()
